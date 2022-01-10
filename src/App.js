1819117f5d76ec97be4d447ba113d5cbe8aa7d22
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-10 10:43:56 +0900

    Third Commit

--- src/App.js
+++ src/App.js
@@ -18,31 +18,6 @@ export default function App() {
         <Link to="/page2">Page2</Link>
         <br />
       </div>
-      <switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              {console.log(url)}
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </switch>
     </BrowserRouter>
   );
 }
