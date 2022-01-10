b5bc667374d785195487451fa26e3ea9dde21018
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-10 12:16:46 +0900

    Section5-21

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
@@ -18,6 +13,7 @@ export default function App() {
         <Link to="/page2">Page2</Link>
         <br />
       </div>
+      <Router />
     </BrowserRouter>
   );
 }
